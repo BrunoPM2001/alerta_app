f810e4d2d15e3ad10ad3d44b54e9db0135a31592
BrunoPM2001 <brunoporras2001@gmail.com>
2023-03-05 05:36:36 -0500

    Pantalla de historial

---
+++ src/screens/history.jsx
@@ -0,0 +1,9 @@
+import { Center, Text } from "native-base";
+
+export default function HistoryScreen() {
+  return (
+    <Center _light={{ bg: "gray.100" }} _dark={{ bg: "black" }} flex={1}>
+      <Text>This is the history screen!</Text>
+    </Center>
+  );
+}
