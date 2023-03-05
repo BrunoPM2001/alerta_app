e81a77895fdf0f6a0605901775c2dd63280804cc
BrunoPM2001 <brunoporras2001@gmail.com>
2023-03-05 04:31:55 -0500

    Contenedor de app incluido

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import Navigator from "./src";
 import AppContainer from "./src/app-container";
 
@@ -10,12 +8,3 @@ export default function App() {
     </AppContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
