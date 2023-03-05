699529e00bc3eadb024a7bdf2f687e3eaaf5224a
BrunoPM2001 <brunoporras2001@gmail.com>
2023-03-05 04:34:12 -0500

    Navegación entre pantallas

---
+++ src/index.jsx
@@ -0,0 +1,21 @@
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import MainScreen from "./screens/main";
+import CreateScreen from "./screens/create";
+
+const Stack = createNativeStackNavigator();
+
+export default function Navigator() {
+  return (
+    <Stack.Navigator
+      initialRouteName="Main"
+      screenOptions={{ headerShown: false }}
+    >
+      <Stack.Screen name="Main" component={MainScreen} />
+      <Stack.Screen
+        name="Crear grupo"
+        options={{ headerShown: true }}
+        component={CreateScreen}
+      />
+    </Stack.Navigator>
+  );
+}
