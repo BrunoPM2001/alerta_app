ed93dc954e9453aab29611321d666f9ff3ac4371
BrunoPM2001 <brunoporras2001@gmail.com>
2023-03-05 23:37:03 -0500

    Lista de historial ok

--- src/components/historialList.jsx
+++ src/components/historialList.jsx
@@ -3,9 +3,19 @@ import { AntDesign } from "@expo/vector-icons";
 
 export default function ListHistory({ id, nombre, estado }) {
   return (
-    <HStack id={id} display="flex" justifyContent="space-between">
+    <HStack
+      id={id}
+      display="flex"
+      justifyContent="space-between"
+      m="auto"
+      w="80%"
+      my={5}
+    >
       <Text>{nombre}</Text>
-      <AntDesign name={estado ? "checkcircleo" : "exclamationcircleo"} />
+      <AntDesign
+        name={estado ? "checkcircleo" : "exclamationcircleo"}
+        size={20}
+      />
     </HStack>
   );
 }
