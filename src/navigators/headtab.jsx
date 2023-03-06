6ffe5a57c1aa6e2945c8b993592b291736dd8dd6
BrunoPM2001 <brunoporras2001@gmail.com>
2023-03-05 19:50:22 -0500

    Tab navigator personalizado

--- src/navigators/headtab.jsx
+++ src/navigators/headtab.jsx
@@ -1,4 +1,5 @@
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { MyTabBar } from "../components/tabBar";
 import HistoryScreen from "../screens/history";
 import MainScreen from "../screens/main";
 
@@ -6,9 +7,9 @@ const Tab = createMaterialTopTabNavigator();
 
 export default function TabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="List" component={MainScreen} />
-      <Tab.Screen name="History" component={HistoryScreen} />
+    <Tab.Navigator tabBar={(props) => <MyTabBar {...props} />}>
+      <Tab.Screen name="Lista" component={MainScreen} />
+      <Tab.Screen name="Historial" component={HistoryScreen} />
     </Tab.Navigator>
   );
 }
