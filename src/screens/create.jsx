aacb4f5bb12e665a3d6398d3c26816e9835903bc
BrunoPM2001 <brunoporras2001@gmail.com>
2023-03-06 22:41:44 -0500

    Pantalla de creación de grupo

--- src/screens/create.jsx
+++ src/screens/create.jsx
@@ -1,9 +1,72 @@
-import { Center, Text } from "native-base";
+import {
+  Container,
+  Input,
+  FormControl,
+  Text,
+  Stack,
+  HStack,
+  ScrollView,
+  Button,
+} from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import data from "../../data_sample/creacion.json";
 
 export default function CreateScreen() {
   return (
-    <Center _light={{ bg: "gray.100" }} _dark={{ bg: "black" }} flex={1}>
-      <Text>This is the creation screen!</Text>
-    </Center>
+    <Container
+      _light={{ bg: "gray.100" }}
+      _dark={{ bg: "black" }}
+      // bgColor="red.500"
+      w="100%"
+      mx="auto"
+      mt="50px"
+      p="10px"
+    >
+      <FormControl>
+        <Stack space={5}>
+          <Stack>
+            <FormControl.Label>
+              <Text fontSize={15} mb={3}>
+                Nombre:
+              </Text>
+            </FormControl.Label>
+            <Input
+              borderColor="amber.400"
+              _focus={{
+                borderColor: "amber.400",
+                bgColor: "transparent",
+              }}
+              fontSize={14}
+              variant="outline"
+              p={2}
+              placeholder="Nombre del grupo"
+            />
+          </Stack>
+          <Stack>
+            <HStack display="flex" justifyContent="space-between" py={5}>
+              <Text fontSize={15}>Lista de endpoints:</Text>
+              <AntDesign name="pluscircleo" size={24} />
+            </HStack>
+            <ScrollView h="60%" w="95%" m="auto">
+              {data.map((item) => {
+                return (
+                  <Text my={3} key={item.nombre} fontSize={16}>
+                    {item.nombre}
+                  </Text>
+                );
+              })}
+            </ScrollView>
+          </Stack>
+          <HStack justifyContent="space-between">
+            <Button w="45%" bgColor="amber.400">
+              Cancelar
+            </Button>
+            <Button w="45%" bgColor="amber.400">
+              Guardar
+            </Button>
+          </HStack>
+        </Stack>
+      </FormControl>
+    </Container>
   );
 }
