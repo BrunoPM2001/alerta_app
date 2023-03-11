1f94ad9468f056e90bf111a3eac15780015e5284
BrunoPM2001 <brunoporras2001@gmail.com>
2023-03-11 08:41:16 -0500

    SwipeList actualizada

--- src/components/swipeList.jsx
+++ src/components/swipeList.jsx
@@ -6,66 +6,68 @@ import { SwipeListView } from "react-native-swipe-list-view";
 export default function SwipeList({ data }) {
   const [listData, setListData] = useState(data);
 
-  const onRowDidOpen = (rowKey) => {
-    console.log("This is open ", rowKey);
-  };
-
-  const closeItem = (rowMap, rowKey) => {
+  const closeRow = (rowMap, rowKey) => {
     if (rowMap[rowKey]) {
       rowMap[rowKey].closeRow();
     }
   };
 
-  const renderItem = ({ item, index }) => {
-    return (
-      <Pressable
-        _light={{ bg: "gray.100" }}
-        _dark={{ bg: "black" }}
-        onPress={() => {
-          console.log("Presionaste un item");
-        }}
-        py={3}
-      >
-        <HStack alignItems="center">
-          <Text key={item.nombre} fontSize={16}>
-            {item.nombre}
-          </Text>
-        </HStack>
-      </Pressable>
-    );
+  const deleteRow = (rowMap, rowKey) => {
+    closeRow(rowMap, rowKey);
+    const newData = [...listData];
+    const prevIndex = listData.findIndex((item) => item.id === rowKey);
+    newData.splice(prevIndex, 1);
+    setListData(newData);
   };
 
-  const renderHiddenItem = (data, rowMap) => {
-    return (
-      <HStack h="100%">
-        <Pressable
-          w="25%"
-          ml="auto"
-          p="2px"
-          bg="red.500"
-          justifyContent="center"
-          onPress={() => {
-            console.log("Deleted ");
-          }}
-        >
-          <VStack alignItems="center" justifyContent="space-around">
-            <MaterialIcons name="delete-outline" size={20} color="white" />
-            <Text color="white" fontSize="xs" fontWeight="medium">
-              Eliminar
-            </Text>
-          </VStack>
-        </Pressable>
-      </HStack>
-    );
+  const onRowDidOpen = (rowKey) => {
+    console.log("This row opened", rowKey);
   };
+
+  const renderItem = (data) => (
+    <Pressable
+      _light={{ bg: "gray.100" }}
+      _dark={{ bg: "black" }}
+      onPress={() => console.log("You touched me")}
+      py={3}
+    >
+      <HStack alignItems="center" pl={2}>
+        <Text>{data.item.nombre}</Text>
+      </HStack>
+    </Pressable>
+  );
+
+  const renderHiddenItem = (data, rowMap) => (
+    <HStack h="100%">
+      <Pressable
+        w="25%"
+        ml="auto"
+        p="2px"
+        bg="red.500"
+        justifyContent="center"
+        onPress={() => {
+          console.log("Deleted ");
+          deleteRow(rowMap, data.item.id);
+        }}
+      >
+        <VStack alignItems="center" justifyContent="space-around">
+          <MaterialIcons name="delete-outline" size={20} color="white" />
+          <Text color="white" fontSize="xs" fontWeight="medium">
+            Eliminar
+          </Text>
+        </VStack>
+      </Pressable>
+    </HStack>
+  );
 
   return (
     <SwipeListView
+      keyExtractor={(item) => item.id}
       data={listData}
       renderItem={renderItem}
       renderHiddenItem={renderHiddenItem}
+      disableRightSwipe
       rightOpenValue={-70}
-      previewRowKey={"0"}
       previewOpenValue={-40}
       previewOpenDelay={3000}
       onRowDidOpen={onRowDidOpen}
